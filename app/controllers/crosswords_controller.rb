e747536ca54098b298a2d866cc2b78f14286d437
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-04-25 00:12:35 -0700

    The crossboard display will coming from passing through default values for horizontal and vertical

--- app/controllers/crosswords_controller.rb
+++ app/controllers/crosswords_controller.rb
@@ -1,4 +1,8 @@
 class CrosswordsController < ApplicationController
   def show
+    @horizontal = "balloon"
+    @vertical = "bat"
+
+    @crossword_display = CrossWords.intersect(horizontal: @horizontal, vertical: @vertical)
   end
 end
