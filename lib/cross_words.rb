5e49d88afd5fafbcbb0c15fa729a5dd4af0e2ca5
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-04-25 00:06:19 -0700

    completed the logic for creating and retu rning the vertical part

--- lib/cross_words.rb
+++ lib/cross_words.rb
@@ -2,9 +2,12 @@ class CrossWords
     def self.intersect(horizontal:, vertical:)
         # Iterate through the horizontal character find each character with its index
         horizontal.each_char.with_index do |h_char, h_idx|
+            binding.pry
             # finding the index of the vertical character that comes from each horizontal character  h_char in the horizontal gird. 
             if (vertical_index = vertical.index(h_char))
-            
+                return vertical[0...vertical_index].chars.map { |c| ' ' * h_idx + c }.join("\n") +
+                    "\n#{horizontal}" +
+                    "\n" + vertical[vertical_index+1..-1].chars.map { |c| ' ' * h_idx + c }.join("\n")
             end 
         end
         "Did not find an intersection"
