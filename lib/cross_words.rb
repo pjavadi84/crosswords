4e3e7aca0656498faff7c88f08ca32cfd3dd474a
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-04-25 00:28:40 -0700

    changed variables to call the downcased version of characters

--- lib/cross_words.rb
+++ lib/cross_words.rb
@@ -5,9 +5,9 @@ class CrossWords
         vertical_downcased = vertical.downcase
 
         # Iterate through the horizontal character find each character with its index
-        horizontal.each_char.with_index do |h_char, h_idx|
+        horizontal_downcased.each_char.with_index do |h_char, h_idx|
             # finding the index of the vertical character that comes from each horizontal character  h_char in the horizontal gird. 
-            if (vertical_index = vertical.index(h_char))
+            if (vertical_index = vertical_downcased.index(h_char))
                 return vertical[0...vertical_index].chars.map { |c| ' ' * h_idx + c }.join("\n") +
                     "\n#{horizontal}" +
                     "\n" + vertical[vertical_index+1..-1].chars.map { |c| ' ' * h_idx + c }.join("\n")
