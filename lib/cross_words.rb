cd0696f6d2b224d3e290724dd0ea5a9eacee374b
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-04-24 23:19:25 -0700

    getting each character and index

--- lib/cross_words.rb
+++ lib/cross_words.rb
@@ -1,5 +1,8 @@
 class CrossWords
     def self.intersect(horizontal:, vertical:)
-        
+        # Iterate through the horizontal character finding each character with its index
+        horizontal.each_char.with_index do |h_char, h_idx|
+            
+        end
     end
 end
