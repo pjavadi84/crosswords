1f86dd7ee464e0244f1731b2c016b4088f407ec4
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-04-24 23:37:43 -0700

    logic to find the index of the vertical character given the character iterated through the horizontal character

--- lib/cross_words.rb
+++ lib/cross_words.rb
@@ -1,8 +1,11 @@
 class CrossWords
     def self.intersect(horizontal:, vertical:)
-        # Iterate through the horizontal character finding each character with its index
+        # Iterate through the horizontal character find each character with its index
         horizontal.each_char.with_index do |h_char, h_idx|
-            
+            # finding the index of the vertical character that comes from each horizontal character  h_char in the horizontal gird. 
+            if (vertical_index = vertical.index(h_char))
+            end
         end
+        "Did not find an intersection"
     end
 end
